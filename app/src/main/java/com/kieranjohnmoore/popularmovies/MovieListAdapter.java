de9a8fa46cee05a40368e25884ca4a13d942d9d0
Kieran Moore <kieranjohnmoore@gmail.com>
2019-04-14 04:18:35 +0100

    Removing unused import

--- app/src/main/java/com/kieranjohnmoore/popularmovies/MovieListAdapter.java
+++ app/src/main/java/com/kieranjohnmoore/popularmovies/MovieListAdapter.java
@@ -1,7 +1,6 @@
 package com.kieranjohnmoore.popularmovies;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
