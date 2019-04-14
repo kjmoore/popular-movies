3db485dee483c74536902b479c3a49b1e33f544e
Kieran Moore <kieranjohnmoore@gmail.com>
2019-04-14 03:36:15 +0100

    Fixing layout on film details

--- app/src/main/java/com/kieranjohnmoore/popularmovies/DetailActivity.java
+++ app/src/main/java/com/kieranjohnmoore/popularmovies/DetailActivity.java
@@ -25,7 +25,7 @@ public class DetailActivity extends AppCompatActivity {
 
     private Movie movie;
 
-    @BindView(R.id.title)
+    @BindView(R.id.movie_title)
     TextView title;
     @BindView(R.id.poster)
     ImageView poster;
